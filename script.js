3719c4d7a7ba4b7fb54982aa6921a4d65a864f17
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-24 12:42:58 -0500

    brought the buttons in

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const axios = require('axios')
 
+const main = document.getElementById('main')
+const addUser = document.getElementById('add-user')
+const double = document.getElementById('double')
+const showMillionaires = document.getElementById('show-millionaires')
+const sort = document.getElementById('sort')
+const calculateWealth = document.getElementById('calculate-wealth')
