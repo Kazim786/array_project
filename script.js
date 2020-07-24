d7447b484da3dbcd8146c1234e045f4d5781e5b8
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-24 12:45:36 -0500

    using axios

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const axios = require('axios')
 
 const main = document.getElementById('main')
-const addUser = document.getElementById('add-user')
-const double = document.getElementById('double')
-const showMillionaires = document.getElementById('show-millionaires')
-const sort = document.getElementById('sort')
-const calculateWealth = document.getElementById('calculate-wealth')
+const addUserBtn = document.getElementById('add-user')
+const doubleBtn = document.getElementById('double')
+const showMillionairesBtn = document.getElementById('show-millionaires')
+const sortBtn = document.getElementById('sort')
+const calculateWealthBtn = document.getElementById('calculate-wealth')
+
+
+let data = []
+
+//get random user and add money. I will use axios
+
+async getRandomUser () {
+   const randomUsers = await axios.get('https://randomuser.me/api')
+   
+}
