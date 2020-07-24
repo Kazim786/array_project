dc7ca6b7d54a26c711bab3ae9bea7d9650ff8954
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-24 13:52:27 -0500

    debugged

--- script.js
+++ script.js
@@ -17,17 +17,33 @@ getRandomUser()
 //get random user and add money. I will use axios
 
 async function getRandomUser () {
-   const randomUsers = await axios.get('https://randomuser.me/api')
+   const randomUsers = await axios.get('https://randomuser.me/api/')
    console.log(randomUsers)
-   const user = randomUsers.data.results[0].name
+//    const user = randomUsers.data.results[0].name
+const user = randomUsers.data.results[0]
    console.log(user)
    const newUser = {
         name: `${user.name.first} ${user.name.last}`,
-        money: Money.floor(Math.random() * 1000000)
+        money: Math.floor(Math.random() * 1000000)
    }
    addData(newUser)
 }
 
 function addData(obj){
-    return data.push(obj)
+    data.push(obj)
+    updateDOM()
+}
+
+//UpdateDOM
+
+function updateDOM(providedData = data){
+    //Clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>'
+
+    providedData.forEach((item) => {
+        const element = document.createElement('div')
+        element.classList.add('person')
+        element.innerHTML = `<strong>${item.name}</strong> ${item.money}`
+        main.appendChild(element)
+    })
 }
